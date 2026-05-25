084434e3ec769751a3491d9c62ce545bf228d028
MishelleVF <mishelle.villarreal@utec.edu.pe>
2026-05-24 22:55:02 -0500

    codigos

---
+++ src/pipelines/components/evaluation.py
@@ -0,0 +1,40 @@
+from kfp.dsl import Dataset, Input, Metrics, Model, Output, component
+
+@component(
+    base_image="gcr.io/deeplearning-platform-release/tf2-cpu.2-6:latest",
+    packages_to_install=[
+        "pandas==1.3.5",
+        "joblib==1.1.0",
+    ],
+)
+
+def choose_best_model(
+    
+    test_dataset: Input[Dataset],
+    decision_tree_model: Input[Model],
+    random_forest_model: Input[Model],
+    best_model: Output[Model],
+    metrics: Output[Metrics],
+):
+    import joblib
+    import pandas as pd
+    from sklearn.metrics import accuracy_score
+
+    test_data = pd.read_csv(test_dataset.path)
+
+    dt = joblib.load(decision_tree_model.path)
+    rf = joblib.load(random_forest_model.path)
+    
+    dt_pred = dt.predict(test_data.drop("Species", axis=1))
+    rf_pred = rf.predict(test_data.drop("Species", axis=1))
+
+    dt_accuracy = accuracy_score(test_data["Species"], dt_pred)
+    rf_accuracy = accuracy_score(test_data["Species"], rf_pred)
+
+    metrics.log_metric("decision_tree_accuracy", dt_accuracy)
+    metrics.log_metric("random_forest_accuracy", rf_accuracy)
+
+    if dt_accuracy > rf_accuracy:
+        joblib.dump(dt, best_model.path)
+    else:
+        joblib.dump(rf, best_model.path)
---
+++ src/pipelines/iris_pipeline.py
@@ -0,0 +1,65 @@
+import sys
+import kfp
+
+sys.path.append("src")
+
+PIPELINE_NAME = "The-Iris-Pipeline-v-mish"
+PIPELINE_ROOT = "gs://mlops-demo-youtube/pipeline_root"
+
+
+@kfp.dsl.pipeline(
+    name=PIPELINE_NAME,
+    pipeline_root = PIPELINE_ROOT,)
+def pipeline(
+    project_id: str,
+    location: str,
+    bd_dataset: str,
+    bq_table: str,
+):
+    from components.data import load_data
+    from components.evaluation import choose_best_model
+    from components.model import decision_tree, random_forest
+    from components.register import upload_model
+
+    data_op = load_data(
+        project_id=project_id,
+        bq_dataset=bd_dataset,
+        bq_table=bq_table,
+    ).set_display_name("Load Data from BQ")
+
+    dt_op = decision_tree(
+        train_dataset=data_op.outputs["train_dataset"]
+        ).set_display_name("Decision Tree")
+    
+    rf_op = random_forest(
+        train_dataset=data_op.outputs["train_dataset"]
+    ).set_display_name("Random Forest")
+
+
+    choose_model_op = choose_best_model(
+        test_dataset=data_op.outputs["test_dataset"],
+        decision_tree_model=dt_op.outputs["output_model"],
+        random_forest_model=rf_op.outputs["output_model"],
+    ).set_display_name("Select Best Model")
+
+
+    upload_model(
+        project_id=project_id,
+        location=location,
+        model=choose_model_op.outputs["best_model"],
+    ).set_display_name("Register model")
+
+# consolidar todo este grafo pero en un yml,
+# esto ayuda que se comunique kfp con vertex ai,
+#  y asi se pueda ejecutar el pipeline desde vertex ai, 
+# sin necesidad de usar el sdk de kfp, y asi 
+# se puede ejecutar el pipeline desde la consola de vertex ai,
+#  o desde gcloud cli, o desde cualquier otro lugar que pueda 
+# ejecutar un comando de gcloud cli.
+if __name__ == "__main__":
+    kfp.compiler.Compiler().compile(
+        pipeline_func=pipeline,
+        package_path=f"pipeline.yml", #se necesita trazabilidad de los pipelines a traves del tiempo
+    )
+
+#artefac register
